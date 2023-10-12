f84b00373d0ee2434f4844dc3724d16c17ee32b7
Redford Ferreras <redfordml@gmail.com>
2023-10-12 00:58:00 -0400

    #25 Bucket should not use Random Proviedr (#26)

--- main.tf
+++ main.tf
@@ -1,20 +1,8 @@
-
-#https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
-
-resource "random_string" "bucket_name" {
-  lower = true
-  upper =false
-  length = 23
-  special = false
- }
-
-
-
 #s3 Rules
 #https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
 
- resource "aws_s3_bucket" "example" {
-   bucket = random_string.bucket_name.result
+ resource "aws_s3_bucket" "website_bucket" {
+   bucket = var.bucket_name
 
   tags = {
 
--- providers.tf
+++ providers.tf
@@ -10,10 +10,7 @@ terraform {
 
 
   required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
+    
     aws = {
       source = "hashicorp/aws"
       version = "5.19.0"
